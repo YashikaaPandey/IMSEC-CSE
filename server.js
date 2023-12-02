ca657e221bc8c95dfa0dfc217162dda97aa95697
YashikaaPandey <115650225+YashikaaPandey@users.noreply.github.com>
2023-12-02 23:33:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use("/uploads", express.static(path.join(__dirname, "/../uploads")));
 app.use(express.static(path.join(__dirname, "/../frontend/build")));
 
 app.get("*", (req, res) => {
-  res.setHeader("Access-Control-Allow-Credentials","true");
+  
   try {
     res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
   } catch (e) {
